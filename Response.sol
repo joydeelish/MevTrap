0a78eeda899f2e56c2a64227e73b1b8da80827b0
joydeelish <joydeelish25@gmail.com>
2026-02-13 09:19:29 +0100

    Update Response.sol

--- Response.sol
+++ Response.sol
@@ -2,34 +2,28 @@
 pragma solidity ^0.8.20;
 
 contract MEVAlertResponder {
-    address public owner;
-    address public caller;
 
     event MEVAlert(
         address indexed pair,
-        uint256 swapCount,
+        uint256 prevPriceE18,
+        uint256 curPriceE18,
+        uint256 changeBps,
         uint256 blockNumber
     );
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    modifier onlyCaller() {
-        require(msg.sender == caller || msg.sender == owner, "not-caller");
-        _;
-    }
-
-    function setCaller(address c) external {
-        require(msg.sender == owner, "!owner");
-        caller = c;
-    }
-
     function respondToMEV(
         address pair,
-        uint256 swaps,
-        uint256 blk
-    ) external onlyCaller {
-        emit MEVAlert(pair, swaps, blk);
+        uint256 prevPriceE18,
+        uint256 curPriceE18,
+        uint256 changeBps,
+        uint256 blockNumber
+    ) external {
+        emit MEVAlert(
+            pair,
+            prevPriceE18,
+            curPriceE18,
+            changeBps,
+            blockNumber
+        );
     }
 }
